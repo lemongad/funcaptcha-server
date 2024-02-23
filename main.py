3fb60f810eac00ea286c2d767ea00850ad5fa806
DG <goodevilbot@gmail.com>
2024-02-23 20:59:52 +0800

    :white_check_mark: Mock Server.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import base64
 import hashlib
+import os
 import time
 from io import BytesIO
 
 from PIL import Image
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from funcaptcha_challenger import predict
 from pydantic import BaseModel
 
@@ -26,6 +27,7 @@ class Task(BaseModel):
 class TaskData(BaseModel):
     clientKey: str
     task: Task
+    softID: str
 
 
 def process_image(base64_image: str, variant: str):
@@ -41,27 +43,40 @@ def process_image(base64_image: str, variant: str):
 
 
 @app.post("/createTask")
-async def create_task(data: TaskData):
-    client_key = data.clientKey
-    task_type = data.task.type
-    image = data.task.image
-    question = data.task.question
+async def create_task(request: Request):
+    try:
+        request_data = await request.json()
+        print(request_data)
+        data = TaskData.parse_obj(request_data)
+        client_key = data.clientKey
+        task_type = data.task.type
+        image = data.task.image
+        question = data.task.question
+    except Exception as e:
+        print(f"Error: {e}")
     ans = {
         "errorId": 0,
         "errorCode": "",
         "status": "ready",
-        "solution": {}
+        "solution": {
+            "label": "arrows_objecthand",
+        }
     }
 
     taskId = hashlib.md5(str(int(time.time() * 1000)).encode()).hexdigest()
     ans["taskId"] = taskId
-    if question in fetcher.supported_models:
-        ans["solution"]["objects"] = [process_image(image, question)]
-    else:
-        ans["errorId"] = 1
-        ans["errorCode"] = "ERROR_TYPE_NOT_SUPPORTED"
-        ans["status"] = "error"
-        ans["solution"]["objects"] = []
+    # 把question写入本地
+    if not os.path.exists('question'):
+        os.makedirs('question')
+    with open(f"question/{taskId}.txt", "w") as f:
+        f.write(question)
+    # if question in fetcher.supported_models:
+    ans["solution"]["objects"] = [process_image(image, '3d_rollball_objects')]
+    # else:
+    #     ans["errorId"] = 1
+    #     ans["errorCode"] = "ERROR_TYPE_NOT_SUPPORTED"
+    #     ans["status"] = "error"
+    #     ans["solution"]["objects"] = []
 
     return ans
 
@@ -70,6 +85,18 @@ async def create_task(data: TaskData):
 async def support():
     # 从文件中读取模型列表
     return fetcher.supported_models
+
+
+@app.post("/getBalance")
+async def balance(request: Request):
+    # 从文件中读取模型列表
+    return {
+        "softBalance": 0,
+        "inviteBalance": 0,
+        "inviteBy": "99",
+        "errorId": 0,
+        "balance": 9999999
+    }
 
 
 @app.exception_handler(Exception)
@@ -85,4 +112,5 @@ async def error_handler(request: Request, exc: Exception):
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=PORT)
